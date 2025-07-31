dce4dc96b1ad66e6a32596223bc3c582af0ee5c4
hdavidkim4326 <hdavidkim@snu.ac.kr>
2025-07-31 18:29:14 +0900

    feat: 세탁 매니저 업로드 페이지   UI 템플릿 (HTML/CSS) 제작

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from laundry_manager.views import main_page
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', main_page, name='main'),
 ]
--- laundry_manager/views.py
+++ laundry_manager/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def main_page(request):
+    return render(request, 'laundry_manager/main.html')
