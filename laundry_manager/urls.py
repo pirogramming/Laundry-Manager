c594b18051df76c1e8b8bc54c3fa1d74e2220720
galgalrobot <okemerald12@naver.com>
2025-08-02 15:53:01 +0900

    보여주는 UI까지 만듬

--- laundry_manager/urls.py
+++ laundry_manager/urls.py
@@ -1,8 +1,13 @@
 # laundry_manager/urls.py
 
+# laundry_manager/urls.py
+
 from django.urls import path
-from .views import laundry_info_view
+from . import views
 
 urlpatterns = [
-    path('laundry/', laundry_info_view, name='laundry_view'),
+    path("", views.stain_guide_view, name="home"),
+    path("laundry/", views.laundry_info_view, name="laundry_view"),
+    path("stain_guide/", views.stain_guide_view, name="stain_guide"),
+    path("stain_detail/<str:slug>/", views.stain_detail_view, name="stain_detail"),
 ]
