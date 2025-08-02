a15fc4d79857eaf56c4bfc097a2dcd43bbde18ff
hdavidkim4326 <hdavidkim@snu.ac.kr>
2025-08-02 15:57:55 +0900

    [feat] 얼룩 선택 페이지 제작 (front)

--- laundry_manager/urls.py
+++ laundry_manager/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.main_page, name='main'),
-    path('upload/', views.upload_page, name='upload'),
+    path('laundy-upload/', views.laundry_upload_page, name='laundry-upload'),
+    path('stain-upload/', views.stain_upload_page, name='stain-upload'),
     path('result/', views.result_page, name='result'),  
+    path('laundry-info/', views.laundry_info_page, name='laundry-info'),  
 ]
