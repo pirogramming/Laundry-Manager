215c0aab3256c5fc850ec3814b52eb9f7d4c3ca2
galgalrobot <okemerald12@naver.com>
2025-08-03 16:39:50 +0900

    오류 해결: 얼룩 안 보이는 오류

--- laundry_manager/urls.py
+++ laundry_manager/urls.py
@@ -14,15 +14,14 @@ urlpatterns = [
     # path("stain_guide/", views.stain_guide_view, name="stain_guide"),
     # path("stain_detail/<str:slug>/", views.stain_detail_view, name="stain_detail"),
     path("laundry/", views.laundry_info_view, name="laundry_view"),
-    #path("stain_guide/", views.stain_guide_view, name="stain_guide"),
+    # path("stain_guide/", views.stain_guide_view, name="stain_guide"),
     path("stain_detail/<str:slug>/", views.stain_detail_view, name="stain_detail"),
-    
     # 프론트 기본 연결
-    path('', views.main_page, name='main'),
-    path('laundy-upload/', views.laundry_upload_page, name='laundry-upload'),
-    path('stain-upload/', views.stain_guide_view, name="stain_guide"),
-    path('result/', views.result_page, name='result'),  
-    path('laundry-info/', views.laundry_info_page, name='laundry-info'),  
-    #path('stain-guide/', views.stain_guide_page, name='stain-guide'),
-    #path('stain-detail/', views.stain_detail_page, name='stain-detail'),
+    path("", views.main_page, name="main"),
+    path("laundy-upload/", views.laundry_upload_page, name="laundry-upload"),
+    path("stain-upload/", views.stain_guide_view, name="stain-upload"),
+    path("result/", views.result_page, name="result"),
+    path("laundry-info/", views.laundry_info_page, name="laundry-info"),
+    # path('stain-guide/', views.stain_guide_page, name='stain-guide'),
+    # path('stain-detail/', views.stain_detail_page, name='stain-detail'),
 ]
