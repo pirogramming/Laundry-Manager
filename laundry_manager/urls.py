2eddf30e7632468cc708f16dd49421c67d365615
ljo1010 <ljo111004@gmail.com>
2025-08-04 11:38:25 +0900

    얼룩제거 가이드 연결

--- laundry_manager/urls.py
+++ laundry_manager/urls.py
@@ -20,10 +20,11 @@ urlpatterns = [
     path('', views.main_page, name='main'),
     path('laundry-upload/', views.upload_view, name='laundry-upload'),
     # path('laundry-upload/', views.upload_and_classify, name='laundry-upload'),
-    path('stain-upload/', views.stain_upload_page, name='stain-upload'),
+    path("stain-upload/", views.stain_guide_view, name="stain-upload"),
+    path("stain-info/", views.stain_info_page, name="stain-info"),
+    # path('stain-upload/', views.stain_upload_page, name='stain-upload'),
     path('result/', views.result_view, name='result'),  
     path('laundry-info/', views.laundry_info_page, name='laundry-info'),  
-
     path('first-info/', views.first_info_view, name='first_info'),
     path('final-info/', views.final_info_view, name='final_info'),
 
