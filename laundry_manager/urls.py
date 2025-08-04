c81713933cd1d0b644e6e530b03fc1f98b91cc07
ljo1010 <ljo111004@gmail.com>
2025-08-04 11:54:31 +0900

    자잘한 오타 수정

--- laundry_manager/urls.py
+++ laundry_manager/urls.py
@@ -4,23 +4,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # legacy
-    # path("", views.upload_and_classify, name="home"),  # 루트 경로는 upload 중심으로 설정
+    path('', views.main_page, name='main'),
     path("upload/", views.upload_and_classify, name="upload"),
     path("uploadimage/", views.upload_view, name="upload_image"),
-    # path("info/", laundry_result_view, name="laundry_info"),
-    # path("result/", laundry_result_view, name="laundry_result"),
-    # path("laundry/", views.laundry_info_view, name="laundry_view"),
-    # path("stain_guide/", views.stain_guide_view, name="stain_guide"),
-    # path("stain_detail/<str:slug>/", views.stain_detail_view, name="stain_detail"),
-    path("laundry/", views.laundry_info_view, name="laundry_view"),
-    # path("stain_guide/", views.stain_guide_view, name="stain_guide"),
+    path("stain-guide/", views.stain_guide_view, name="stain-guide"),
     path("stain_detail/<str:slug>/", views.stain_detail_view, name="stain_detail"),
-    # 프론트 기본 연결
-    path('', views.main_page, name='main'),
+    path("laundry/", views.laundry_info_view, name="laundry_view"),
     path('laundry-upload/', views.upload_view, name='laundry-upload'),
     # path('laundry-upload/', views.upload_and_classify, name='laundry-upload'),
-    path("stain-upload/", views.stain_guide_view, name="stain-upload"),
+    path("stain-upload/", views.stain_guide_view, name="stain-upload"), 
     path("stain-info/", views.stain_info_page, name="stain-info"),
     # path('stain-upload/', views.stain_upload_page, name='stain-upload'),
     path('result/', views.result_view, name='result'),  
