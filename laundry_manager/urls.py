37475f9552a97291850f3a0e152610be44e62864
galgalrobot <okemerald12@naver.com>
2025-08-05 13:48:12 +0900

    [얼룩 상세정보 나오는 페이지 및 laundry_temp_json 폴더 삭제]

--- laundry_manager/urls.py
+++ laundry_manager/urls.py
@@ -4,20 +4,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main_page, name='main'),
+    path("", views.main_page, name="main"),
     path("upload/", views.upload_and_classify, name="upload"),
     path("uploadimage/", views.upload_view, name="upload_image"),
     path("stain-guide/", views.stain_guide_view, name="stain-guide"),
-    path("stain_detail/<str:slug>/", views.stain_detail_view, name="stain_detail"),
     path("laundry/", views.laundry_info_view, name="laundry_view"),
-    path('laundry-upload/', views.upload_view, name='laundry-upload'),
+    path("laundry-upload/", views.upload_view, name="laundry-upload"),
     # path('laundry-upload/', views.upload_and_classify, name='laundry-upload'),
-    path("stain-upload/", views.stain_guide_view, name="stain-upload"), 
+    path("stain-upload/", views.stain_guide_view, name="stain-upload"),
     path("stain-info/", views.stain_info_page, name="stain-info"),
     # path('stain-upload/', views.stain_upload_page, name='stain-upload'),
-    path('result/', views.result_view, name='result'),  
-    path('laundry-info/', views.laundry_info_page, name='laundry-info'),  
-    path('first-info/', views.first_info_view, name='first_info'),
-    path('final-info/', views.final_info_view, name='final_info'),
-
+    path("result/", views.result_view, name="result"),
+    path("laundry-info/", views.laundry_info_page, name="laundry-info"),
+    path("first-info/", views.first_info_view, name="first_info"),
+    path("final-info/", views.final_info_view, name="final_info"),
 ]
