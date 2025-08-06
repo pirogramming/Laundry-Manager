ed39af01bcd4378ff7088e225d326b3f71886c2d
galgalrobot <okemerald12@naver.com>
2025-08-07 02:28:21 +0900

    [임시템플릿 만들어서 정보 연결되는지 확인]

--- laundry_manager/urls.py
+++ laundry_manager/urls.py
@@ -22,4 +22,7 @@ urlpatterns = [
     path('final-info/', views.final_info_view, name='final_info'),
     # path('laundry-upload/', UploadView.as_view(), name='laundry-upload'),
     path('laundry-info-v1/', views.laundry_info_view1, name='laundry-info-v1'),
+
+    #권준희 - 백과사전 URL 연결
+    path('dictionary/', views.dictionary, name='dictionary'),
 ]
