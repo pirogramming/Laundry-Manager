0dc0afefb6c648603dc3800021b3bf3468a8779d
ljo1010 <ljo111004@gmail.com>
2025-08-12 15:08:45 +0900

    [merege] 일단은 충돌부분 해결완료 했음

--- laundry_manager/views/pages.py
+++ laundry_manager/views/pages.py
@@ -23,3 +23,33 @@ def stain_guide_page(request):
 
 def stain_detail_page(request):
     return render(request, "laundry_manager/stain_detail.html")
+
+
+def login_page(request):
+    return render(request, "laundry_manager/login.html")
+def dictionary_page(request):
+    return render(request, "laundry_manager/dictionary.html")
+def main2_page(request):
+    return render(request, "laundry_manager/main2.html")
+def profile_page(request):
+    return render(request, "laundry_manager/profile.html")
+def settings_page(request):
+    return render(request, "laundry_manager/settings.html")
+
+def settings_developer_page(request):
+    return render(request, "laundry_manager/settings-developer.html")
+def settings_faq_page(request):
+    return render(request, "laundry_manager/settings-faq.html")
+def settings_opensource_page(request):
+    return render(request, "laundry_manager/settings-opensource.html")
+def settings_terms_page(request):
+    return render(request, "laundry_manager/settings-terms.html")
+def settings_privacy_page(request):
+    return render(request, "laundry_manager/settings-privacy.html")
+
+def account_settings_page(request):
+    return render(request, "laundry_manager/account-settings.html")
+def contact_settings_page(request):
+    return render(request, "laundry_manager/contact-settings.html")
+def record_settings_page(request):
+    return render(request, "laundry_manager/record-settings.html")
